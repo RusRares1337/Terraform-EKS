7bce0a52c67c55fb7f8b2f8c9acccc130fe67b89
Rus Rares <rusrares1997@gmail.com>
2024-02-07 17:52:26 +0200

    added tf module for vpc and configured necessary arguments

---
+++ vpc.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+    region = "eu-west-3"
+}
+
+
+variable vpc_cidr_block {}
+variable private_subnet_cidr_block {}
+variable public_subnet_cidr_block {}
+
+data "aws_availability_zone" "azs" {}
+
+module "myapp-vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.1"
+
+  name = "myapp-vpc"
+  cidr = var.vpc_cidr_block
+
+  azs = data.aws_availability_zone.azs.names
+  private_subnets = var.private_subnet_cidr_block
+  public_subnets = var.public_subnet_cidr_block
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+}
